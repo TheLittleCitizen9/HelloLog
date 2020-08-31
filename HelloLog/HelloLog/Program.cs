14df37c7dbf3f2780e66c2365e678cf49376c5c1
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-31 17:53:01 +0300

    Write log with Serilog

---
+++ HelloLog/HelloLog/Program.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+using System;
+
+namespace HelloLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            log.Information("Hello Serilog!");
+        }
+    }
+}
