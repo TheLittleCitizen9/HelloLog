54ae99802ee60079d934edf28ad8bc262f43ccf6
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-31 20:24:36 +0300

    finished first two exercises

---
+++ HelloLog/HelloLog/LogicRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace HelloLog
+{
+    public class LogicRunner
+    {
+        public void RunLogic(string message)
+        {
+            Random rnd = new Random();
+
+            LogEventLevel logLevel = (LogEventLevel)(LogLevel)rnd.Next(6);
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            logger.Write(logLevel, message);
+        }
+    }
+}
--- HelloLog/HelloLog/Program.cs
+++ HelloLog/HelloLog/Program.cs
@@ -1,5 +1,7 @@
-﻿using Serilog;
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
 using System;
+using System.IO;
 
 namespace HelloLog
 {
@@ -7,8 +9,22 @@ namespace HelloLog
     {
         static void Main(string[] args)
         {
-            var log = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-            log.Information("Hello Serilog!");
+            //var log = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            //log.Information("Hello Serilog!");
+
+            //var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            //.AddJsonFile("appsettings.json")
+            //.Build();
+
+            //var logger = new LoggerConfiguration()
+            //    .ReadFrom.Configuration(configuration)
+            //    .CreateLogger();
+
+            //logger.Information("Hello Serilog!");
+
+            LogicRunner logicRunner = new LogicRunner();
+
+            logicRunner.RunLogic("Hello Serilog!");
         }
     }
 }
